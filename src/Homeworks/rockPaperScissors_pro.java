781d8815f049fe8caf734836bbc8355c54af37b2
Ksenia <49162294+xeniakasjan@users.noreply.github.com>
2022-08-27 09:51:42 +0300

    27/08 Pro version of Rock-Paper-Scissors

---
+++ src/Homeworks/rockPaperScissors_pro.java
@@ -0,0 +1,39 @@
+package Homeworks;
+import java.util.Scanner;
+
+public class rockPaperScissors_pro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter Player's 1 choice");
+        String player1 = scanner.nextLine().toLowerCase().trim();
+
+        System.out.println("Please enter Player's 2 choice");
+        String player2 = scanner.nextLine().toLowerCase().trim();
+
+        //System.out.println(rockPaperScissors_pro(player1,player2));
+    }
+
+    public static String rockPaperScissors(String player1, String player2) {
+        String answer;
+        if (!checkInputValid(player1, player2)) {
+            return "Input values are not valid";
+        }
+
+        if (player1.equals("paper") && player2.equals("rock")
+                || player1.equals("scissors") && player2.equals("paper")
+                || player1.equals("rock") && player2.equals("scissors")) {
+            answer = "Player 1 wins";
+        } else if (player1.equals(player2)) {
+            answer = "Its a tie!";
+        } else {
+            answer = "Player 2 wins!";
+        }
+        return answer;
+    }
+
+        public static boolean checkInputValid (String player1, String player2){
+            return player1.matches("rock|scissors|paper") && player2.matches("rock|scissors|paper");
+        }
+
+    }
